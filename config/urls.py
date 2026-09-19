ba17768bc0922d5608f377d7b1e05217df6b7320
ANTHONY8652 <githinjianthony720@gmail.com>
2026-09-19 19:53:57 +0300

    Fixes

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from accounts.views import RegisterView, SetRoleView
@@ -35,3 +37,6 @@ urlpatterns = [
     path("api/users/<int:pk>/role/", SetRoleView.as_view()),
     path("api/", include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
